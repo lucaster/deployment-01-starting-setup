e7b6821764c059a4c0058e3b6fa0b59eb999b557
luca <luca.cavagnoli@gmail.com>
2021-02-16 21:06:44 +0100

    pushed image, ssh

--- script.sh
+++ script.sh
@@ -1,2 +1,10 @@
-docker build -t node-dep-example .
-docker run -d --rm --name node-dep -p 80:80 node-dep-example
+docker build -t udemy-academind-docker-module09 .
+docker tag udemy-academind-docker-module09 lucaster/udemy-academind-docker-module09
+docker push lucaster/udemy-academind-docker-module09
+docker run -d --rm --name udemy-academind-docker-module09 -p 80:80 udemy-academind-docker-module09
+
+chmod 400 udemy-academind-docker-module09.pem
+ssh -i "udemy-academind-docker-module09.pem" ec2-user@3.236.19.101
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
